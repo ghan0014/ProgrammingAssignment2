286b002f5f42e9200b256a5342bcf0f5cbe61df6
Obaid Ghani <obaid@Obaids-MacBook-Pro.local>
2015-02-21 15:57:42 -0600

    function desc added

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,8 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## The functions below will cache the inverse of a matrix, which may 
+## be potentially resource intensive and or time consuming calculations
+
 
 ## Write a short comment describing this function
 
